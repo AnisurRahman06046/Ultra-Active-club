6164029011f838988bc8c1ab52a520ea549adb01
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-09-29 02:16:50 +0600

    key added to remove the warning

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import Activity from '../Activity/Activity';
 
 const Activities = () => {
     const [activities,setActivities]=useState([]);
     useEffect(()=>{
         fetch('data.json')
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>setActivities(data))
     },[])
     return (
         <div>
             <h2>Select Today's Exercise</h2>
+            {
+                activities.map(activity=><Activity
+                key={activity.id}></Activity>)
+            }
         </div>
     );
 };
