74a4b92ed3565c260ee462dea65c16cf5520e638
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-09-29 05:46:38 +0600

    my code has been crashed thats why i am doing it from the start again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// import './App.css';
-import Body from './components/Body/Body';
+import './App.css';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <Body></Body>
+        <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Header.css'
-import logo from '../../Logo/Logo.png'
+import Logo from '../../Logo/Logo.png'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <img src={logo} alt="" />
-            <h3>Power Zone</h3>
+        <div className='d-flex align-items-center'>
+            <img  style={{height:'100px',width:'100px'}} src={Logo} alt="" />
+            <h2>Power Zone</h2>
         </div>
     );
 };
