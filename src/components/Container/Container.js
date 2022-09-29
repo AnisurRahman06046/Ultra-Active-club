e51841ee486d7c753fecae3e04979ed83d968633
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-09-29 06:00:28 +0600

    header added

---
+++ src/components/Container/Container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Container = () => {
+    return (
+        <div>
+             <div className='container text-center'>
+                <div className='row border border-primary'>
+                    <div className='col-8 border border-secondary'>
+                             <h1>activities container</h1>
+                    </div>
+                    <div className='col-4'>
+                            <h1>list container</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo.png'
 
 const Header = () => {
     return (
-        <div className='d-flex align-items-center'>
+        <div className='d-flex align-items-center container mt-5'>
             <img  style={{height:'100px',width:'100px'}} src={Logo} alt="" />
             <h2>Power Zone</h2>
         </div>
